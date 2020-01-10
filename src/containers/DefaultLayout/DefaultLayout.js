53b95b9bbb27ecd30fbc667d7e0865ff1d84b16a
Gabriele <gabriele.belluardo@outlook.it>
2020-01-10 12:59:16 +0100

    add prettier and eslint config

--- src/containers/DefaultLayout/DefaultLayout.js
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -4,7 +4,6 @@ import { Container } from "reactstrap";
 
 import {
   //  AppAside,
-  AppBreadcrumb,
   AppFooter,
   AppHeader,
   AppSidebar,
@@ -20,7 +19,7 @@ import navigation from "../../_nav";
 import routes from "../../routes";
 
 //const DefaultAside = React.lazy(() => import("./DefaultAside"));
-const DefaultFooter = React.lazy(() => import('./DefaultFooter'));
+const DefaultFooter = React.lazy(() => import("./DefaultFooter"));
 const DefaultHeader = React.lazy(() => import("./DefaultHeader"));
 
 class DefaultLayout extends Component {
