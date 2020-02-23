1e722ea3b3e290fcf1ee4dd61e0544d78f6200b4
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2020-02-23 21:58:29 +0100

    MyMap component

--- src/map/index.js
+++ src/map/index.js
@@ -1,6 +1,6 @@
 import L from 'leaflet'
 import { Map, Marker, Circle, Popup, TileLayer } from 'react-leaflet'
-import React from 'react'
+import React, { Component } from 'react'
 
 export const bikeIcon = new L.Icon({
   iconUrl: 'https://raw.githubusercontent.com/stelosca96/marta_map_component/master/public/taurus_icon.png?token=AKTACIBKBKQFUSV2NVBXF7S6LPU24',
@@ -35,10 +35,7 @@ function name_pos(name){
 }
 //
 //
-function new_pos() {
-  // this.taurusX_pos[0] += 0.00001;
-  // this.taurusX_pos[1] += 0.00001;
-}
+
 
 // updateMarker = ({new_pos}) => {
 //     //update
@@ -49,36 +46,90 @@ function new_pos() {
 //     });
 // };
 
- let taurusX_pos = [40.433212, -117.053714];
-const MyMap = ({ position_name}) => {
-  let v = name_pos(position_name);
-  let position = v[0];
-  let zoom_level = v[1];
-  setInterval(new_pos, 1000, 100);
-  return (
-    <Map center={position} zoom={zoom_level}>
-      <TileLayer
-        url="https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=5adf6850b4034fb3a71eff6eeab5d818"
-        attribution='&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        apikey="5adf6850b4034fb3a71eff6eeab5d818"
-        minZoom={4}
-        maxNativeZoom={17}
-        maxZoom={17}
-      />
-      <Circle center={[40.393431, -117.043385]} radius={200} color={"yellow"}>
-        <Popup><b>Start zone</b></Popup>
-      </Circle>
-      <Circle center={[40.466338, -117.062507]} radius={200} color={"red"}>
-        <Popup><b>Timing zone</b></Popup>
-      </Circle>
-      <Circle center={[40.476676, -117.065328]} radius={200} color={"blue"}>
-        <Popup><b>Catching zone</b></Popup>
-      </Circle>
-      <Marker key="taurusX" position={taurusX_pos} icon={bikeIcon}>
-        <Popup><b>TaurusX</b></Popup>
-      </Marker>
-    </Map>
-  )
-};
+
+
+class MyMap extends Component {
+  _isMounted = false
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      position_name: 'bm',
+      bike_position: [40.433212, -117.053714],
+      visible: false,
+      visible_video: false,
+      visible_rasp: false,
+    }
+    setTimeout(this.new_pos(), 1000)
+  }
+
+  new_pos() {
+    console.log("QUAAA")
+
+      let pos = this.state.bike_position
+      pos[0] += 0.00001
+      pos[1] += 0.00001
+      let newState = {
+        ...this.state,
+        bike_position: [pos[0], pos[1]],
+      }
+      this.setState(newState)
+  }
+
+  componentDidMount() {
+    this._isMounted = true
+    // this.reloadStatus()
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false
+  }
+
+  reloadStatus() {
+    // SocketIoHelper.getSettings(settings => {
+    //   if (JSON.stringify(this.state.settings) !== JSON.stringify(settings)) {
+    //     this.setState({ settings })
+    //   }
+    // })
+    // SocketIoHelper.getState(state => {
+    //   this.setState({ state })
+    // })
+  }
+
+  // updateView = () => {
+  //   this.reloadStatus()
+  // }
+
+  render() {
+    let v = name_pos(this.state.position_name);
+    let position = v[0];
+    let zoom_level = v[1];
+    return (
+      <Map center={position} zoom={zoom_level}>
+        <TileLayer
+          url="https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=5adf6850b4034fb3a71eff6eeab5d818"
+          attribution='&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          apikey="5adf6850b4034fb3a71eff6eeab5d818"
+          minZoom={4}
+          maxNativeZoom={17}
+          maxZoom={17}
+        />
+        <Circle center={[40.393431, -117.043385]} radius={200} color={"yellow"}>
+          <Popup><b>Start zone</b></Popup>
+        </Circle>
+        <Circle center={[40.466338, -117.062507]} radius={200} color={"red"}>
+          <Popup><b>Timing zone</b></Popup>
+        </Circle>
+        <Circle center={[40.476676, -117.065328]} radius={200} color={"blue"}>
+          <Popup><b>Catching zone</b></Popup>
+        </Circle>
+        <Marker key="taurusX" position={this.state.bike_position} icon={bikeIcon}>
+          <Popup><b>TaurusX</b></Popup>
+        </Marker>
+      </Map>
+    )
+  }
+}
 
 export default MyMap
