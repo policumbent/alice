c1105b07fcbc2dadbdff9f5590bba27782f4e84d
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2021-02-25 18:07:01 +0100

    update react-scripts

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ const username = process.env.REACT_APP_USER
 const password = process.env.REACT_APP_PASS
 
 const dataService = {
-  isLogged: function (): boolean {
+  isLogged: function(): boolean {
     const jwt = localStorage.getItem('jwt')
     if (jwt == null) {
       return false
@@ -18,28 +18,28 @@ const dataService = {
     return true
   },
 
-  getRole: function (): boolean {
+  getRole: function(): boolean {
     if (!this.isLogged()) {
       return false
     }
-    const jwt = localStorage.getItem('jwt') || ""
+    const jwt = localStorage.getItem('jwt') || ''
     const jwtParse = JSON.parse(atob(jwt.split('.')[1]))
     return jwtParse.roles[0]
   },
 
-  getJwt: function (): string | null {
+  getJwt: function(): string | null {
     return localStorage.getItem('jwt')
   },
 
-  setJwt: function (token: string): void {
+  setJwt: function(token: string): void {
     localStorage.setItem('jwt', token)
   },
 
-  removeJwt: function (): void {
+  removeJwt: function(): void {
     localStorage.removeItem('jwt')
   },
 
-  login: function (username: string, password: string): void {
+  login: function(username: string, password: string): void {
     const v = { username, password }
     fetch(host + '/authenticate', {
       method: 'POST',
@@ -53,60 +53,56 @@ const dataService = {
       .catch(err => console.log(err))
   },
 
-  getHeaders: function (): Headers {
+  getHeaders: function(): Headers {
     let headers = new Headers()
     if (dataService.isLogged())
       headers.set('Authorization', 'Bearer ' + dataService.getJwt())
     return headers
   },
 
-  getBike: async function () {
+  getBike: async function() {
     const url = `${host}/v3/alice/config`
 
     return await fetch(url)
       .then(r => {
-        if (r.status === 200)
-          return r;
-        throw new Error('Network response was not ok');
-      }
-      )
+        if (r.status === 200) return r
+        throw new Error('Network response was not ok')
+      })
       .then(r => r.json())
-      .then(data => { return data })
+      .then(data => {
+        return data
+      })
       .catch(error => console.log(error))
   },
 
-  getHistory: function (cb: any, bike: string) {
+  getHistory: function(cb: any, bike: string) {
     const url = `${host}/v3/activities/last/${bike}?n=60`
 
     fetch(url, { method: 'GET', headers: dataService.getHeaders() })
       .then(r => {
-        if (r.status === 200)
-          return r;
-        throw new Error('Network response was not ok');
-      }
-      )
+        if (r.status === 200) return r
+        throw new Error('Network response was not ok')
+      })
       .then(r => r.json())
       .then(data => cb(data))
       .catch(error => console.log(error))
   },
 
-  getConfig: function (cb: any) {
+  getConfig: function(cb: any) {
     const url = `${host}/v3/alice/config`
 
     fetch(url)
       .then(r => {
-        if (r.status === 200)
-          return r;
-        throw new Error('Network response was not ok');
-      }
-      )
+        if (r.status === 200) return r
+        throw new Error('Network response was not ok')
+      })
       .then(r => r.json())
       .then(data => cb(data))
       .catch(error => console.log(error))
   },
 
   // ritorna una lista con l'ultimo dato meteo di ogni stazione
-  getWeather: function (cb: any) {
+  getWeather: function(cb: any) {
     // socket.on('weather_response', weather => cb(JSON.parse(weather)))
     const url = `${host}/v3/weather/last`
     fetch(url, { method: 'GET', headers: dataService.getHeaders() })
@@ -116,22 +112,20 @@ const dataService = {
   }, // ritorna una lista con l'ultimo dato meteo di ogni stazione
 
   // ritorna l'ultimo dato meteo di una specifica stazione
-  getWeatherSingleStation: function (cb: any, id: number) {
+  getWeatherSingleStation: function(cb: any, id: number) {
     // socket.on('weather_response', weather => cb(JSON.parse(weather)))
     const url = `${host}/v3/weather/last/${id}`
     fetch(url, { method: 'GET', headers: dataService.getHeaders() })
       .then(r => {
-        if (r.status === 200)
-          return r;
-        throw new Error('Network response was not ok');
-      }
-      )
+        if (r.status === 200) return r
+        throw new Error('Network response was not ok')
+      })
       .then(r => r.json())
       .then(data => cb(data))
       .catch(error => console.log(error))
   },
 
-  getComments: function (cb: any) {
+  getComments: function(cb: any) {
     const url = `${host}/v3/alice/comments`
     fetch(url)
       .then(comments => comments.json())
@@ -139,20 +133,17 @@ const dataService = {
       .catch(error => console.log(error))
   },
 
-  getData: function (cb: any, bike: string) {
+  getData: function(cb: any, bike: string) {
     const url = `${host}/v3/activities/last/${bike}`
     fetch(url, { method: 'GET', headers: dataService.getHeaders() })
       .then(r => {
-        if (r.status === 200)
-          return r;
-        throw new Error('Network response was not ok');
-      }
-      )
+        if (r.status === 200) return r
+        throw new Error('Network response was not ok')
+      })
       .then(r => r.json())
       .then(data => cb(data[0]))
       .catch(error => console.log(error))
   },
-
 }
 
 export default dataService
