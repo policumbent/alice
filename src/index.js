5bbf10490f51972f4405fdc1be72cd4efdef328d
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2021-03-11 12:24:13 +0100

    rm: support to IE

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import 'preact/devtools'
 
-import 'react-app-polyfill/ie9' // For IE 9-11 support
-import 'react-app-polyfill/ie11' // For IE 11 support
-import './polyfill'
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 
